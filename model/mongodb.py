688fb1ed51e0e18e67f1cf34ef5e39bd4c09f57a
jay900411 <119254312+jay900411@users.noreply.github.com>
2023-08-09 14:08:00 +0800

    Update mongodb.py

--- model/mongodb.py
+++ model/mongodb.py
@@ -5,7 +5,7 @@ stockDB = 'mydb'
 dbname = 'howard-good31'
 
 def constructor_stock():
-    client = MongoClient('mongodb://jay900411:r224697146@ac-jcnyyjh-shard-00-00.zwmifuf.mongodb.net:27017,ac-jcnyyjh-shard-00-01.zwmifuf.mongodb.net:27017,ac-jcnyyjh-shard-00-02.zwmifuf.mongodb.net:27017/?ssl=true&replicaSet=atlas-6vruxq-shard-0&authSource=admin&retryWrites=true&w=majority')
+    client = MongoClient('mongodb://jay900411:Qq00451211@ac-jcnyyjh-shard-00-00.zwmifuf.mongodb.net:27017,ac-jcnyyjh-shard-00-01.zwmifuf.mongodb.net:27017,ac-jcnyyjh-shard-00-02.zwmifuf.mongodb.net:27017/?ssl=true&replicaSet=atlas-6vruxq-shard-0&authSource=admin&retryWrites=true&w=majority')
     db = client[stockDB]
     return db
 
