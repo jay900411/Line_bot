b1b5ba10759a89f8a773eeadae0c2e3e5922561a
jay900411 <119254312+jay900411@users.noreply.github.com>
2023-11-22 22:10:42 +0800

    Update line_bot_api.py

--- line_bot_api.py
+++ line_bot_api.py
@@ -3,6 +3,7 @@ from linebot import (LineBotApi, WebhookHandler, exceptions)
 from linebot.exceptions import (InvalidSignatureError)
 from linebot.models import *
 import random
+import time
 # 必須放上自己的Channel Access Token
 line_bot_api = LineBotApi('t3GglwAI98AmV9rf0s1uHXILs/QYPlEjIV7g7VaurSGBg7Hb1+iw5jD4qNGWi5xFmn0K/yPg1vBPZmpddxG6RytUvyR7qDXhRbpSptqgBNcHA39zQpeKCW4+wvoydpGE5//ojrHuZBaOQnP9r0itCgdB04t89/1O/w1cDnyilFU=')
 # 必須放上自己的Channel Secret
